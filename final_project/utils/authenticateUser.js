ad7a25be18de797686666d815d426c02599f5d3c
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-07-06 15:51:13 +0200

    feat: implement authentication mechanism

---
+++ final_project/utils/authenticateUser.js
@@ -0,0 +1,21 @@
+function auth(req, res, next) {
+  // Check if the user is logged in and has valid access token
+  if (req.session.authorization) {
+    let token = req.session.authorization['accessToken'];
+
+    // Verify JWT token
+    jwt.verify(token, 'access', (err, user) => {
+      if (err)
+        return res.status(403).json({
+          message: 'User not authenticated',
+        });
+
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(403).json({ message: 'User not logged in' });
+  }
+}
+
+module.exports = auth;
